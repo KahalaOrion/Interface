4f0ae1fdabea713892f21b335dbbc4cb8fb1bd0a
KahalaOrion <78617741+KahalaOrion@users.noreply.github.com>
2021-04-17 17:58:37 -0400

    test 5

--- test-05/test-05.js
+++ test-05/test-05.js
@@ -1,26 +1,72 @@
-//microphone input + getting volume level The Coding Train 17.8: Microphone Input - p5.js Sound Tutorial 
+//graphing amplitude The Coding Train 17.9: Sound Visualization: Graphing Amplitude - p5.js Sound Tutorial
 
-var mic;
+
+var song;
+var button;
+var amp;
+
+var volhistory = [];
+
+function preload(){
+	song = loadSound('assets/startrek.mp3')
+
+}
 
 
 
 function setup(){
+	angleMode(DEGREES);
 	let cnv = createCanvas(200, 200);
-  	cnv.mousePressed(userStartAudio);
-	mic = new p5.AudioIn();
-	mic.start();
+  	button = createButton("play/pause")
+  	button.mousePressed(togglePlaying)
+
+  	amp = new p5.Amplitude();
 
 
+}
 
+function togglePlaying(){
+	if(!song.isPlaying()){
+	song.play();
+	button.html("pause");
+	} else {
+		song.pause();
+		button.html("play");
+	}
 }
 
 
 function draw(){
 	background(0);
+	var vol = amp.getLevel();
+	volhistory.push(vol);
+	stroke(255)
+	noFill()
 
-	var vol = mic.getLevel();
-	ellipse (100,100, 200, vol*600);
-	console.log(vol)
+
+	translate(width/2, height/2);
+	beginShape();
+	for (var i = 0; i < 360; i++){
+		var r = map(volhistory[i], 0, 1, 10, 200);;
+		var x = r * cos(i);
+		var y = r * sin(i);
+		// var y = map(volhistory[i], 0, 1, height/2, 0);
+		vertex (x, y);
+	}
+	endShape();
+
+	// beginShape();
+	// for (var i = 0; i < volhistory.length; i++){
+	// 	var y = map(volhistory[i], 0, 1, height/2, 0);
+	// 	vertex (i, y);
+	// }
+	// endShape();
+
+	if (volhistory.length > 360){
+		volhistory.splice(0,1);
+	}
+
+	// ellipse (100,100, 200, vol*400);
 	
 }
 
