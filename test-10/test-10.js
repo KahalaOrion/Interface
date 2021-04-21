a1e8a5bd7aa09f3b80089f06082ad60e2d0f95f2
KahalaOrion <78617741+KahalaOrion@users.noreply.github.com>
2021-04-21 15:15:39 -0400

    :)

--- test-10/test-10.js
+++ test-10/test-10.js
@@ -91,7 +91,7 @@ function draw(){
   redish = color(vol*90,100,100)
   greenish = color(90+(vol*90),100,100)
   turquoise = color(180+(vol*90),100,100)
-  whites = color(100)
+  whites = color(0,0, vol*100)
 
   colors = [rainbow, redish, greenish, turquoise, whites];
   console.log(n)
@@ -103,16 +103,17 @@ function draw(){
 
     let spectrum = fft.analyze();
 
-  for (let i = 0; i< spectrum.length; i++){
-    let x = map(i, 0, spectrum.length, 0, width-500);
+
+   for (let i = 0; i< spectrum.length; i++){
+    let x = map(i, 0, spectrum.length, 0, width+80);
     let h = -height + map(spectrum[i], 0, 255, height, 0);
-    ellipse(x*4, -h, width / spectrum.length, -h )
+    ellipse(x-75, -h, width / spectrum.length, -h )
   }
 
     for (let i = 0; i< spectrum.length; i++){
-    let x = map(i, 0, spectrum.length, 0, width-500);
+    let x = map(i, 0, spectrum.length, 0, width);
     let h = -height + map(spectrum[i], 0, 255, height, 0);
-    ellipse(x*4, height, width / spectrum.length, h )
+    ellipse(x, height, width / spectrum.length, h )
   }
 
   
@@ -125,7 +126,7 @@ function draw(){
   let waveform = fft.waveform();
   
   beginShape();
-  stroke(0)
+  stroke(colors[4])
   for (let i = 0; i < waveform.length; i++){
     let x = map(i, 0, waveform.length, 0, width+100);
     let y = map( waveform[i], -1,1, 0, height);
